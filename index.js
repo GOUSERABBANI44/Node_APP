24705949d85da10a439450f10ffe59330ea9caea
rabbani <109769095+GOUSERABBANI44@users.noreply.github.com>
2024-03-25 10:00:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello APPS DEPLOYER This is Gouserabbani Shaik!\n'
+  const msg = 'Hello March 6th team today holi. so, happy holi to everyone and play safe without any chemicals colors!\n'
   res.end(msg);
 });
 
