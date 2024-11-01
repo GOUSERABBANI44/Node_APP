dc742fa5b6a9b537106e08c053c919c493f460cb
rabbani <109769095+GOUSERABBANI44@users.noreply.github.com>
2024-11-01 09:26:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello hai how are you and happy birthday Rakesh Gulla and have a beautiful GF in this year!\n'
+  const msg = 'Hello hai how are you Happy Diwali!\n'
   res.end(msg);
 });
 
